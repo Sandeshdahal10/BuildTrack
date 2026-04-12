963bfcbb5143b357b4e89482a6c4027052f3051a
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-04-12 11:03:43 +0545

    Fixed the error.

--- src/main/java/com/buildtrack/service/ClientService.java
+++ src/main/java/com/buildtrack/service/ClientService.java
@@ -1,11 +1,4 @@
 package com.buildtrack.service;
 
 public class ClientService {
-    private final ClientDAO clientDAO= new ClientDAO();
-
-    public List<Client> getAll() {return clientDAO.findAll();}
-    public Client getByUserId(int userId) {return clientDAO.findByUserId(userId);}
-    public Client getById(int id) {return clientDAO.findById(id);}
-    public boolean create(client client) {return clientDAO.insert(client)>0;}
-    public boolean update(client client) {return clientDAO.update(client);}
 }
